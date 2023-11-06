e1a7f8fbb313a50fe7d2a7bdd912b2b9242f2fa2
Ehsan Faramarzi <ehsan.faramarzi@turck.com>
2023-11-06 10:03:59 +0100

    cd opt

--- Nik.Common/EnvironmentHelper.cs
+++ Nik.Common/EnvironmentHelper.cs
@@ -2,43 +2,81 @@
 
 public class EnvironmentHelper : IEnvironmentHelper
 {
-    private const string DotnetVariableName = "DOTNET_ENVIRONMENT";
-    private const string HostingVariableName = "ASPNETCORE_ENVIRONMENT";
-    private string[] ValidEnvironmentNames = new string[] { "Development", "Staging", "Production" };
-    private string environmentName = string.Empty;
+    private const string DotnetVariable = "DOTNET_ENVIRONMENT";
+    private const string AspNetCoreVariable = "ASPNETCORE_ENVIRONMENT";
+
+    private const string Development = "Development";
+    private const string Staging = "Staging";
+    private const string Production = "Production";
+
+    private const string AppSettingsFile = "appsettings.json";
+
+    private string[] ValidEnvironments = new string[] { Development, Staging, Production };
+    private string activeEnvironment = string.Empty;
 
     public IConfigurationRoot CreateConfiguration()
     {
         var environment = GetEnvironmentName();
-        Environment.SetEnvironmentVariable(DotnetVariableName, environment);
-        Environment.SetEnvironmentVariable(HostingVariableName, environment);
+        Environment.SetEnvironmentVariable(DotnetVariable, environment);
+        Environment.SetEnvironmentVariable(AspNetCoreVariable, environment);
 
         var configuration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json")
-            .AddJsonFile($"appsettings.{environment}.json")
+            .AddJsonFile(AppSettingsFile)
+            .AddJsonFile(GetEnvironmentFile(environment))
             .Build();
 
         return configuration;
     }
 
+    private static string GetEnvironmentFile(string environment)
+    {
+        return $"appsettings.{environment}.json";
+    }
+
     public string GetEnvironmentName()
     {
-        if (string.IsNullOrWhiteSpace(environmentName))
+        if (string.IsNullOrWhiteSpace(activeEnvironment))
         {
             var configuration = new ConfigurationBuilder()
-               .AddJsonFile("appsettings.json")
+               .AddJsonFile(AppSettingsFile)
                .Build();
 
             var tempName = configuration.GetValue<string>("EnvironmentName")!;
-            environmentName = tempName;
-            if (!ValidEnvironmentNames.Contains(tempName))
+            if (string.IsNullOrWhiteSpace(tempName))
             {
-                environmentName = "Development";
+                tempName = Development;
+            }
+            if (!ValidEnvironments.Contains(tempName))
+            {
                 throw new Exception($"Unknown environment name: {tempName}");
             }
+            activeEnvironment = tempName;
+
+            DeleteOtherSettingsFiles();
         }
 
-        return environmentName;
+        return activeEnvironment;
+    }
+
+    private void DeleteOtherSettingsFiles()
+    {
+        IEnumerable<string> otherFiles = ValidEnvironments
+            .Except(new string[] { activeEnvironment })
+            .Select(env => GetEnvironmentFile(env));
+
+        foreach (var file in otherFiles)
+        {
+            if (File.Exists(file))
+            {
+                try
+                {
+                    File.Delete(file);
+                }
+                catch
+                {
+                }
+            }
+        }
     }
 
     public bool IsProduction() => GetEnvironmentName().ToLower() == "production";
