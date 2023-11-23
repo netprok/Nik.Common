bba00d0ef177bfa25a8a2a525b1f5b355696f214
netprok <netpro@email.com>
2023-11-23 16:30:13 +0100

    pkg

--- Nik.Common/ServicesExtensions.cs
+++ Nik.Common/ServicesExtensions.cs
@@ -2,19 +2,9 @@
 
 public static class ServicesExtensions
 {
-    public static IServiceCollection UseEnvironmentHelper(this IServiceCollection services)
+    public static IServiceCollection UseCommon(this IServiceCollection services)
     {
         services.AddSingleton<IEnvironmentHelper, EnvironmentHelper>();
-
-        IEnvironmentHelper environmentHelper = new EnvironmentHelper();
-        var configuration = environmentHelper.CreateConfiguration();
-        services.AddSingleton(configuration);
-
-        return services;
-    }
-
-public static IServiceCollection UseCommon(this IServiceCollection services)
-    {
         services.AddSingleton<IStringFormatter, EnglishStringFormatter>();
         services.AddSingleton<IHashGenerator, SHA256HashGenerator>();
         services.AddSingleton<IObjectMapper, ObjectMapper>();
