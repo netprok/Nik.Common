da1782acb5f2778719c60bec97fe9aaeb9a300aa
Ehsan Faramarzi <netpro@email.com>
2023-12-20 19:07:33 +0100

    IListSplitter added

---
+++ Nik.Common.Abstractions/IListSplitter.cs
@@ -0,0 +1,6 @@
+﻿namespace Nik.Common.Abstractions;
+
+public interface IListSplitter
+{
+    List<List<T>> SplittByCount<T>(List<T> source, int count);
+}
---
+++ Nik.Common/ListSplitter.cs
@@ -0,0 +1,13 @@
+﻿namespace Nik.Common;
+
+public sealed class ListSplitter : IListSplitter
+{
+    public List<List<T>> SplittByCount<T>(List<T> source, int count)
+    {
+        return source
+           .Select((x, i) => new { Index = i, Value = x })
+           .GroupBy(x => x.Index / count)
+           .Select(x => x.Select(v => v.Value).ToList())
+           .ToList();
+    }
+}
