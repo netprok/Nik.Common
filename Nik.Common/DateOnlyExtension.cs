4d23b5f430eb77c0c2a0367392707203027b2524
Ehsan Faramarzi <netpro@email.com>
2024-02-24 16:51:14 +0100

    dateonle extension updated

--- Nik.Common/DateOnlyExtension.cs
+++ Nik.Common/DateOnlyExtension.cs
@@ -2,11 +2,6 @@
 
 public static class DateOnlyExtension
 {
-    public static DateOnly Now(this DateOnly _)
-    {
-        return System.DateOnly.FromDateTime(DateTime.Now);
-    }
-
     public static DateOnly DateOnly(this DateTime dateTime)
     {
         return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
