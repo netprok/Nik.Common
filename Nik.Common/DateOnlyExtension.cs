2e7da96490b9831842695e6afad2a58e924afa7c
Ehsan Faramarzi <netpro@email.com>
2024-02-24 16:37:48 +0100

    dateonly extension added

---
+++ Nik.Common/DateOnlyExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace Nik.Common;
+
+public static class DateOnlyExtension
+{
+    public static DateOnly Now(this DateOnly _)
+    {
+        return System.DateOnly.FromDateTime(DateTime.Now);
+    }
+
+    public static DateOnly DateOnly(this DateTime dateTime)
+    {
+        return new DateOnly(dateTime.Year, dateTime.Month, dateTime.Day);
+    }
+}
